709e535d6b5cc7bc2b824d76d809f126ac08f187
Zavhorodnii Boris <zavgorodnijboris188@gmail.com>
2023-02-10 20:35:31 +0200

    Work in progress

---
+++ Flask_app/models.py
@@ -0,0 +1,47 @@
+from typing import List, Optional
+from sqlalchemy import String, ForeignKey, Table, Column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+student_course = Table('student_course', Base.metadata,
+                       Column('student_id', ForeignKey('student.id'), primary_key=True),
+                       Column('course_name', ForeignKey('course.name'), primary_key=True))
+
+
+class CourseModel(Base):
+    __tablename__ = 'course'
+
+    name: Mapped[str] = mapped_column(String(32), primary_key=True)
+    description: Mapped[Optional[str]] = mapped_column(String(1024))
+
+    students: Mapped[List['StudentModel']] = relationship(secondary=student_course, back_populates='courses')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class StudentModel(Base):
+    __tablename__ = 'student'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(32))
+    last_name: Mapped[str] = mapped_column(String(32))
+
+    group_name: Mapped[Optional[str]] = mapped_column(ForeignKey('group.name'))
+
+    courses: Mapped[List[CourseModel]] = relationship(secondary=student_course, back_populates='students')
+
+
+class GroupModel(Base):
+    __tablename__ = 'group'
+
+    name: Mapped[str] = mapped_column(String(5), primary_key=True)
+
+
+
+
+
---
+++ Flask_app/populate_db.py
@@ -0,0 +1,132 @@
+from random import choice, randint
+from string import ascii_uppercase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import Base, StudentModel, GroupModel, CourseModel
+from engine_data import engine_data
+
+
+class CreateDBData:
+    f_name = ('John', 'Alfred', 'Thomas', 'Mike', 'Steve', 'Eric', 'Kenny', 'Kyle', 'James', 'Jack',
+              'Edward', 'Bob', 'Desmond', 'Charlie', 'Hugo', 'Ben', 'Benjamine', 'Jake', 'Bred', 'Boris')
+
+    l_name = ('Lock', 'Black', 'Gray', 'Twain', 'Rodgers', 'Cartman', 'McCormic', 'Broflowsky', 'Cook', 'Shepard',
+              'Callen', 'Big', 'Hume', 'Green', 'Raise', 'Churchill', 'Lienuss', 'Stone', 'Butter', 'Cool')
+
+    courses = ('Math', 'Biology', 'Physik', 'Chemistry', 'Literature',
+               'Psychology', 'Programming', 'Cybersecurity', 'Engineering', 'Law')
+
+    @staticmethod
+    def create_groups():
+        groups = []
+
+        for _ in range(20):
+            chars = choice(ascii_uppercase) + choice(ascii_uppercase)
+            nums = str(randint(0, 99)).zfill(2)
+
+            group = '-'.join([chars, nums])
+
+            groups.append(group)
+
+        return groups
+
+    @classmethod
+    def create_students(cls):
+        students = []
+
+        for _ in range(20):
+            name = (choice(cls.f_name), choice(cls.l_name))
+            students.append(name)
+
+        return students
+
+    @classmethod
+    def create_course_models(cls, session: Session) -> list:
+        course_models = []
+
+        for course in cls.courses:
+            course_models.append(CourseModel(name=course))
+
+        session.add_all(course_models)
+
+        return course_models
+
+    @classmethod
+    def create_group_models(cls, session: Session) -> list:
+        group_models = []
+        groups = cls.create_groups()
+
+        for group in groups:
+            group_models.append(GroupModel(name=group))
+
+        session.add_all(group_models)
+        session.commit()
+
+        return group_models
+
+    @classmethod
+    def create_student_model(cls, session: Session, students: list, courses: list, group_name=None):
+        first_name, last_name = students.pop()
+
+        courses_amount = randint(1, 3)
+        students_courses = []
+
+        for _ in range(courses_amount):
+            course = choice(courses)
+            while course in students_courses:
+                course = choice(courses)
+
+            students_courses.append(course)
+
+        session.add(StudentModel(first_name=first_name,
+                                 last_name=last_name,
+                                 group_name=group_name,
+                                 courses=students_courses
+                                 ))
+
+    @classmethod
+    def set_up_db(cls):
+        engine = create_engine(engine_data)
+        Base.metadata.drop_all(engine)
+        Base.metadata.create_all(engine)
+        session = Session(bind=engine)
+
+        return session
+
+    @classmethod
+    def close_db(cls, session: Session):
+        session.close()
+
+    @classmethod
+    def fill_db_data(cls):
+        session = cls.set_up_db()
+
+        groups = cls.create_group_models(session)
+        courses = cls.create_course_models(session)
+        students = cls.create_students()
+
+        for group in groups:
+            for _ in range(10, 30):
+                if students:
+                    cls.create_student_model(session=session, students=students,
+                                             courses=courses, group_name=group.name)
+                else:
+                    break
+
+        # if no more groups left
+        for _ in range(len(students)):
+            cls.create_student_model(session=session, students=students, courses=courses)
+
+
+        # I also tried this code, but it fills only 100 elements to DB. I have no idea why
+        # for _ in range(len(students)):
+        #     cls.create_student_model(session=session, students=students, courses=courses)
+
+
+        session.commit()
+
+        cls.close_db(session)
+
+
+filler = CreateDBData()
+filler.fill_db_data()
